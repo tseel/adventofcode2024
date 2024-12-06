45d8387da6ad4bd850e8bcfd4bba7084e2a7e56c
Travis Seel <travis.seel.1@gmail.com>
2024-12-06 17:47:58 -0600

    Day 5

---
+++ Day5Lib/Day5.cs
@@ -0,0 +1,77 @@
+﻿using Interfaces;
+
+namespace Day5Lib;
+
+public class Day5 : IAOCDay
+{
+   public int Day => 5;
+
+   private readonly PageSorter _sorter = new();
+   private readonly List<List<int>> _pages = [];
+
+   private class PageSorter : IComparer<int>
+   {
+      public Dictionary<int, HashSet<int>> Rules { get; } = new();
+
+      public int Compare(int x, int y)
+      {
+         if (Rules.TryGetValue(x, out var xRules) && xRules.Contains(y)) return -1;
+         if (Rules.TryGetValue(y, out var yRules) && yRules.Contains(x)) return 1;
+         return 0;
+      }
+   }
+   public Day5()
+   {
+      ParseData();
+   }
+
+   private void ParseData()
+   {
+      using var file = File.OpenRead("Day5.txt");
+      using StreamReader sr = new(file);
+
+      // Parse rules
+      while (sr.ReadLine() is { Length: > 0 } line)
+      {
+         var rule = line.Split('|').Select(int.Parse).ToList();
+         if (!_sorter.Rules.ContainsKey(rule[0])) _sorter.Rules.Add(rule[0], []);
+         _sorter.Rules[rule[0]].Add(rule[1]);
+      }
+
+      // Parse pages
+      while (sr.ReadLine() is { } line)
+      {
+         _pages.Add(line.Split(',').Select(int.Parse).ToList());
+      }
+   }
+
+   public long Part1()
+   {
+      long medianSum = 0;
+      foreach (var page in _pages)
+      {
+         var pageSorted = page[..];
+         pageSorted.Sort(_sorter);
+         if (pageSorted.SequenceEqual(page))
+         {
+            medianSum += page[page.Count / 2];
+         }
+      }
+      return medianSum;
+   }
+
+   public long Part2()
+   {
+      long medianSum = 0;
+      foreach (var page in _pages)
+      {
+         var pageSorted = page[..];
+         pageSorted.Sort(_sorter);
+         if (!pageSorted.SequenceEqual(page))
+         {
+            medianSum += pageSorted[pageSorted.Count / 2];
+         }
+      }
+      return medianSum;
+   }
+}
--- Runner/Program.cs
+++ Runner/Program.cs
@@ -1,5 +1,5 @@
-﻿using Day4Lib;
+﻿using Day5Lib;
 
-var day = new Day4();
+var day = new Day5();
 Console.WriteLine($"Day {day.Day} Part 1: {day.Part1()}");
 Console.WriteLine($"Day {day.Day} Part 2: {day.Part2()}");
